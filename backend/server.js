e5c9ab4c8f85cf012ca319eccd7564973fd5bed5
Shantanu Agarwal <byteninja010@gmail.com>
2025-09-20 19:28:08 +0530

    Changed CORS policy

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://zephyra-phi.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
 app.use(express.json());
