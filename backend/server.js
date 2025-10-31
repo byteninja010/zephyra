7794a5f377e62019d292b82ee472198f5ae9206c
Shantanu Agarwal <byteninja010@gmail.com>
2025-10-31 19:51:16 +0530

    Added Firebase Hosting and GCP

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://zephyra-phi.vercel.app'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://zephyra-phi.vercel.app','https://zephyra-cbe14.web.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
   }
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://zephyra-phi.vercel.app'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://zephyra-phi.vercel.app','https://zephyra-cbe14.web.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
